162137d6fbd41b8f669ac9738b376e21954fc455
thebighairedguy <basarnq2@outlook.com>
2022-07-05 19:48:44 +0500

    updated todo

--- todo/style-guide.md
+++ todo/style-guide.md
@@ -52,3 +52,186 @@ tick: <i class="fa-solid fa-circle-check"></i>
 no-tick: <i class="fa-regular fa-circle"></i>
 
 cross: <i class="fa-solid fa-xmark"></i>
+
+const checked = Array.from(
+document.getElementsByName("checkedtick")
+);
+
+checked.forEach((checkedTask) => {
+checkedTask.parentNode.parentNode.remove();
+});
+
+numberShown.innerText = active.length + " items left";
+
+---
+
+console.log(1);
+
+const form = document.forms["task-form"];
+const input = form["task-input"];
+const tasksdiv = document.getElementById("tasks-div");
+const numberOfTasks = document.getElementById("tasks-div");
+const numberShown = document.getElementById("total-tasks");
+
+//let x = 0;
+
+var completed = [];
+var active = [];
+var Pcompleted = [];
+var Pactive = [];
+
+form.addEventListener("submit", (e) => {
+e.preventDefault();
+tasksdiv.innerHTML +=
+'<div class="task-line"><div class="checkmark-i"><i class="fa-regular fa-circle" onclick="checktask(this)" name="uncheckedtick"></i></div><p name="task-text-name">' +
+input.value +
+'</p><i class="fa-solid fa-xmark" onclick="removetask(this)"></i></div>';
+
+Pcompleted.length = 0;
+Pactive.length = 0;
+
+completed = Array.from(document.getElementsByName("checkedtick"));
+completed.forEach((element) => {
+Pcompleted.push(element.parentNode.parentNode);
+});
+
+active = Array.from(document.getElementsByName("uncheckedtick"));
+active.forEach((element) => {
+Pactive.push(element.parentNode.parentNode);
+});
+
+numberShown.innerText = active.length + " items left";
+});
+
+function removetask(element) {
+element.parentElement.remove();
+
+//also need to count elements from Pactive or active for # of items left
+
+//show all Parrays
+
+//remove line through array and then remove Parray
+
+Pcompleted.length = 0;
+Pactive.length = 0;
+
+completed = Array.from(document.getElementsByName("checkedtick"));
+completed.forEach((element) => {
+Pcompleted.push(element.parentNode.parentNode);
+});
+
+active = Array.from(document.getElementsByName("uncheckedtick"));
+active.forEach((element) => {
+Pactive.push(element.parentNode.parentNode);
+});
+}
+
+function checktask(element2) {
+element2.parentNode.nextElementSibling.style.color = "darkgray";
+element2.parentNode.nextElementSibling.style.textDecoration =
+"line-through";
+
+element2.parentElement.innerHTML =
+'<i class="fa-solid fa-circle-check" onclick="unchecktask(this)" name="checkedtick"></i>';
+
+Pcompleted.length = 0;
+Pactive.length = 0;
+
+completed = Array.from(document.getElementsByName("checkedtick"));
+completed.forEach((element) => {
+Pcompleted.push(element.parentNode.parentNode);
+});
+
+active = Array.from(document.getElementsByName("uncheckedtick"));
+active.forEach((element) => {
+Pactive.push(element.parentNode.parentNode);
+});
+}
+
+function unchecktask(element3) {
+element3.parentNode.nextElementSibling.style.color =
+"darkslategray";
+element3.parentNode.nextElementSibling.style.textDecoration =
+"none";
+
+element3.parentElement.innerHTML =
+'<i class="fa-regular fa-circle" onclick="checktask(this) name="uncheckedtick""></i>';
+
+Pcompleted.length = 0;
+Pactive.length = 0;
+
+completed = Array.from(document.getElementsByName("checkedtick"));
+completed.forEach((element) => {
+Pcompleted.push(element.parentNode.parentNode);
+});
+
+active = Array.from(document.getElementsByName("uncheckedtick"));
+active.forEach((element) => {
+Pactive.push(element.parentNode.parentNode);
+});
+}
+
+function clearCompleted() {
+completed.forEach((element) => {
+element.parentNode.parentNode.remove();
+});
+
+x = 0;
+
+Pcompleted.length = 0;
+// Pactive.length = 0;
+
+completed = Array.from(document.getElementsByName("checkedtick"));
+completed.forEach((element) => {
+Pcompleted.push(element.parentNode.parentNode);
+});
+
+// active = Array.from(document.getElementsByName("uncheckedtick"));
+// active.forEach((element) => {
+// Pactive.push(element.parentNode.parentNode);
+// });
+}
+
+function all(button) {}
+
+function funcActive(button) {
+const buttonColor = getComputedStyle(button).color;
+
+if (buttonColor == "rgb(169, 169, 169)") {
+button.style.color = "rgb(0, 0, 255)";
+
+    completed.forEach((element) => {
+      element.parentNode.parentNode.remove();
+    });
+
+} else if (buttonColor == "rgb(0, 0, 255)") {
+button.style.color = "rgb(169, 169, 169)";
+
+    Pcompleted.forEach((element) => {
+      tasksdiv.appendChild(element);
+    });
+    // Pcompleted.length = 0;
+
+}
+}
+
+function funcCompleted(button) {
+const buttonColor = getComputedStyle(button).color;
+
+if (buttonColor == "rgb(169, 169, 169)") {
+button.style.color = "rgb(0, 0, 255)";
+
+    active.forEach((element) => {
+      element.parentNode.parentNode.remove();
+    });
+
+} else if (buttonColor == "rgb(0, 0, 255)") {
+button.style.color = "rgb(169, 169, 169)";
+
+    Pactive.forEach((element) => {
+      tasksdiv.appendChild(element);
+    });
+    // Pactive.length = 0;
+
+}
+}
--- todo/todo.js
+++ todo/todo.js
@@ -1,77 +1,181 @@
-console.log(1);
+console.log("todo.js loaded.");
+//variable declarations
 
 const form = document.forms["task-form"];
-const input = form["task-input"];
 const tasksdiv = document.getElementById("tasks-div");
-const numberOfTasks = document.getElementById("tasks-div");
 const numberShown = document.getElementById("total-tasks");
+const activeButton = document.getElementById("active");
+const completedButton = document.getElementById("completed");
+const activeButtonColor = getComputedStyle(activeButton).color;
+const completedButtonColor = getComputedStyle(completedButton).color;
 
-let x = 0;
+var Pcompleted = [];
+var Pactive = [];
+
+//tg_num will give id to each task line
+
+let tg_num = 0;
+
+//hover functions
+
+function hoverOn(element) {
+  element.lastChild.style.display = "block";
+}
+
+function hoverOff(element) {
+  element.lastChild.style.display = "none";
+}
+
+//main functions
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  tasksdiv.innerHTML +=
-    '<div class="task-line"><div class="checkmark-i"><i class="fa-regular fa-circle" onclick="checktask(this)" name="uncheckedtick"></i></div><p name="task-text-name">' +
-    input.value +
-    '</p><i class="fa-solid fa-xmark" onclick="removetask(this)"></i></div>';
+  const input = form["task-input"];
 
-  numberShown.innerText =
-    numberOfTasks.childElementCount + " items left";
+  //first x=0. Then, if Completed is on, make x=1, if x=1 then first disable it
+
+  tg_num++;
+
+  tasksdiv.innerHTML += `<div id="task_num_${tg_num}" class="task-line" onmouseenter="hoverOn(this)" onmouseleave="hoverOff(this)" ><div class="checkmark-i"><i class="fa-regular fa-circle" onclick="checktask(this)" name="uncheckedtick"></i></div><p class="task-text-name">${input.value}</p><i class="fa-solid fa-xmark" name="cross" onclick="removetask(this)"></i></div>`;
+
+  Pactive.push(tasksdiv.lastChild);
+
+  //not Pcompleted because we arent changing that here, we are only adding to Pactive here
+
+  console.log(Pactive.length);
+  numberShown.innerText = Pactive.length + " items left";
+
+  //Now re-enable Completed if x=1 it was enabled beforehand
 });
 
 function removetask(element) {
-  element.parentElement.remove();
+  //check which array the element is in and delete from array
+  for (let x = 0; x < Pactive.length; x++) {
+    if (Pactive[x].id == element.parentNode.id) {
+      Pactive.splice(x, 1);
+    }
+  }
+  for (let x = 0; x < Pcompleted.length; x++) {
+    if (Pcompleted[x].id == element.parentNode.id) {
+      Pcompleted.splice(x, 1);
+    }
+  }
 
-  numberShown.innerText =
-    numberOfTasks.childElementCount + " items left";
+  //delete task line
+  element.parentNode.remove();
+
+  console.log(Pactive.length);
+  numberShown.innerText = Pactive.length + " items left";
 }
 
-function checktask(element2) {
-  element2.parentNode.nextElementSibling.style.color = "darkgray";
-  element2.parentNode.nextElementSibling.style.textDecoration =
-    "line-through";
+function checktask(element) {
+  //store element in const for if reference
+  const store_element = element.parentNode.parentNode;
 
-  element2.parentElement.innerHTML =
-    '<i class="fa-solid fa-circle-check" onclick="unchecktask(this)" name="checkedtick"></i>';
+  //delete from Pactive and add to Pcompleted
+  for (let x = 0; x < Pactive.length; x++) {
+    if (Pactive[x].id == store_element.id) {
+      //edit css properties of line
+      element.parentNode.nextSibling.style.color = "darkgray";
+      element.parentNode.nextSibling.style.textDecoration =
+        "line-through";
 
-  x -= 1;
-  numberShown.innerText =
-    numberOfTasks.childElementCount + x + " items left";
+      //add to Pcompleted and remove from Pactive
+      Pcompleted.push(element.parentNode.parentNode);
+      Pactive.splice(x, 1);
+
+      //lastly change it to tick
+      element.parentElement.innerHTML =
+        '<i class="fa-solid fa-circle-check" onclick="unchecktask(this)" name="checkedtick"></i>';
+    }
+  }
+
+  console.log(Pactive.length);
+  numberShown.innerText = Pactive.length + " items left";
 }
 
-function unchecktask(element3) {
-  element3.parentNode.nextElementSibling.style.color =
-    "darkslategray";
-  element3.parentNode.nextElementSibling.style.textDecoration =
-    "none";
+function unchecktask(element) {
+  //store element in const for if reference
+  const store_element = element.parentNode.parentNode;
 
-  element3.parentElement.innerHTML =
-    '<i class="fa-regular fa-circle" onclick="checktask(this) name="uncheckedtick""></i>';
+  //delete from Pcompleted and add to Pactive
+  for (let x = 0; x < Pcompleted.length; x++) {
+    if (Pcompleted[x].id == store_element.id) {
+      //edit css properties of line
+      element.parentNode.nextSibling.style.color = "rgb(47, 79, 79)";
+      element.parentNode.nextSibling.style.textDecoration = "none";
 
-  x += 1;
-  numberShown.innerText =
-    numberOfTasks.childElementCount + x + " items left";
+      //add to Pcompleted and remove from Pactive
+      Pactive.push(element.parentNode.parentNode);
+      Pcompleted.splice(x, 1);
+
+      //lastly change it to tick
+      element.parentElement.innerHTML =
+        '<i class="fa-regular fa-circle" onclick="checktask(this)" name="uncheckedtick"></i>';
+    }
+  }
+
+  console.log(Pactive.length);
+  numberShown.innerText = Pactive.length + " items left";
 }
 
 function clearCompleted() {
-  const checked = Array.from(
-    document.getElementsByName("checkedtick")
-  );
+  //remove taskdiv then add only active and delete Pcompleted
 
-  checked.forEach((checkedTask) => {
-    checkedTask.parentNode.parentNode.remove();
+  activeButton.click();
+
+  tasksdiv.innerHTML = "";
+  Pactive.forEach((element) => {
+    tasksdiv.appendChild(element);
   });
+
+  Pcompleted.length = 0;
 }
 
-function active() {
-  //  const completed = Array.from(
-  //    document.getElementsByName("checkedtick")
-  // );
-  //  const Pcompleted = [];
-  //  completed.forEach((element) => {
-  //    Pcompleted.push(element.parentNode.parentNode);
-  //  });
-  //  completed.forEach((element) => {
-  //    element.parentNode.parentNode.remove();
-  //  });
+function filter_out(button) {
+  tasksdiv.innerHTML = "";
+
+  //change color of button using if
+  const buttonColor = getComputedStyle(button).color;
+
+  if (buttonColor == "rgb(0, 0, 255)") {
+    button.style.color = "rgb(169, 169, 169)";
+  } else if (buttonColor == "rgb(169, 169, 169)") {
+    button.style.color = "rgb(0, 0, 255)";
+  }
+
+  const activeButtonColor = getComputedStyle(activeButton).color;
+  const completedButtonColor =
+    getComputedStyle(completedButton).color;
+
+  if (
+    activeButtonColor == "rgb(0, 0, 255)" &&
+    completedButtonColor == "rgb(0, 0, 255)"
+  ) {
+    Pactive.forEach((element) => {
+      tasksdiv.appendChild(element);
+    });
+    Pcompleted.forEach((element) => {
+      tasksdiv.appendChild(element);
+    });
+  } else if (
+    activeButtonColor == "rgb(0, 0, 255)" &&
+    completedButtonColor == "rgb(169, 169, 169)"
+  ) {
+    Pactive.forEach((element) => {
+      tasksdiv.appendChild(element);
+    });
+  } else if (
+    activeButtonColor == "rgb(169, 169, 169)" &&
+    completedButtonColor == "rgb(0, 0, 255)"
+  ) {
+    Pcompleted.forEach((element) => {
+      tasksdiv.appendChild(element);
+    });
+  } else if (
+    activeButtonColor == "rgb(169, 169, 169)" &&
+    completedButtonColor == "rgb(169, 169, 169)"
+  ) {
+    console.log("both filtered out.");
+  }
 }
