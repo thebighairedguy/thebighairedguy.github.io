8bb7bbca93bfc95195c62ac9779f53623b6cf19a
thebighairedguy <basarnq2@outlook.com>
2022-08-24 19:57:41 +0500

    full stack feather.app

--- latest-project.js
+++ latest-project.js
@@ -1,8 +1,6 @@
-const latest_proj_button = document.getElementById(
-  "latest-proj-button"
-);
+const latest_proj_button = document.getElementById("latest-proj-button");
 
-const latest_proj = "/react-todo/react-todo.html";
+const latest_proj = "https://webdev-c3-project-bhg.herokuapp.com/login.html";
 
 if (document.title == "Basar's Profile") {
   latest_proj_button.setAttribute("href", `${latest_proj}`);
